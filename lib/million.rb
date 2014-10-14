c11aabf6b45d65cf1d1a393c663376942756b845
scott123454 <scott123454@hotmail.co.uk>
2014-10-14 21:01:16 +0100

    added hash

--- lib/million.rb
+++ lib/million.rb
@@ -1,3 +1,17 @@
+NUMBER_TO_WORDS={
+	1 => 'one',
+	2 => 'two',
+	3 => 'three',
+	4 => 'four',
+	5 => 'five',
+	6 => 'six',
+	7 => 'seven',
+	8 => 'eight',
+	9 => 'nine',
+	10 => 'ten'
+}
+
+
 def returns_word_number(number)
-	'one'
+	NUMBER_TO_WORDS[number]
 end
--- spec/million_spec.rb
+++ spec/million_spec.rb
@@ -7,6 +7,26 @@ describe 'Print words one to one million' do
 		expect(returns_word_number(1)).to eq 'one'
 	end
 
+	it 'prints "two" for 2' do
+		expect(returns_word_number(2)).to eq 'two'
+	end
+
+	it 'prints "ten" for 10' do
+		expect(returns_word_number(10)).to eq 'ten'
+	end
+
+	it 'prints "eleven" for 11' do
+		expect(returns_word_number(2)).to eq 'eleven'
+	end
+
+	xit 'prints "twenty" for 20' do
+		expect(returns_word_number(2)).to eq 'two'
+	end
+
+	xit 'prints "one hunded" for 100' do
+		expect(returns_word_number(2)).to eq 'two'
+	end
+
 end
 
 	
