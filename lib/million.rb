a50455fdb19a9adf0632aa46e710348eba248418
scott123454 <scott123454@hotmail.co.uk>
2014-10-14 20:39:56 +0100

    added first method

---
+++ lib/million.rb
@@ -0,0 +1,3 @@
+def returns_word_number(number)
+	'one'
+end
---
+++ spec/million_spec.rb
@@ -0,0 +1,12 @@
+require 'million.rb'
+
+describe 'Print words one to one million' do
+
+
+	it 'prints "one" for 1' do
+		expect(returns_word_number(1)).to eq 'one'
+	end
+
+end
+
+	
